b37aa3b1c03f7f527a8d64b97309a726d7a2d253
paul-saac <johnpaul.saac.legaspi@gmail.com>
2025-06-15 21:33:08 +0800

    asd

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
-import {
-  getAuth,
-  GoogleAuthProvider,
-  signInWithPopup
-} from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js";
 
 const sidebar = document.getElementById('sidebar')
 
