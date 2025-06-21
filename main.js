17537b11a3bd092e7da4869944a728479fa0d18d
paul-saac <johnpaul.saac.legaspi@gmail.com>
2025-06-22 04:53:56 +0800

    sidebar selectors

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
-
+// SIDEBAR RESPONSIVE
 const sidebar = document.getElementById('sidebar')
 
 function toggleSidebar() {  
     sidebar.classList.toggle('show')
 }
 
+// SIDEBAR SELECTOR
+const sidebarLinks = document.querySelectorAll('aside ul li a');
+
+sidebarLinks.forEach(link => {
+    link.addEventListener('click', function(e) {
+        // Remove 'active' from all
+        sidebarLinks.forEach(l => l.parentElement.classList.remove('active'));
+        // Add 'active' to the clicked one
+        this.parentElement.classList.add('active');
+    });
+});
