74c6f96978624a10450ea8cebf9d34c9358e8fd5
paul-saac <johnpaul.saac.legaspi@gmail.com>
2025-11-09 21:16:48 +0800

    nye

--- auth/login.js
+++ auth/login.js
@@ -1,21 +1,5 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
-import { getAuth, signInWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
-import { getFirestore} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
-
-const firebaseConfig = {
-    apiKey: "AIzaSyBzVudrq_MK5GXiVIlrsDRV5XtGPZ8K5ro",
-    authDomain: "library-sys-53d31.firebaseapp.com",
-    projectId: "library-sys-53d31",
-    storageBucket: "library-sys-53d31.firebasestorage.app",
-    messagingSenderId: "593530003665",
-    appId: "1:593530003665:web:25dbd6cd027a2f191225f0",
-    measurementId: "G-HBYTYZBV6B"
-  };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-
+import { auth, app} from "../main.js";
+import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
 
 // LOGIN
 const emailinput = document.getElementById('email');
--- main.js
+++ main.js
@@ -1,5 +1,22 @@
-import { signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
-// import { auth, app } from './scripts/firebase-config.js';
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+// import { getFirestore} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBzVudrq_MK5GXiVIlrsDRV5XtGPZ8K5ro",
+    authDomain: "library-sys-53d31.firebaseapp.com",
+    projectId: "library-sys-53d31",
+    storageBucket: "library-sys-53d31.firebasestorage.app",
+    messagingSenderId: "593530003665",
+    appId: "1:593530003665:web:25dbd6cd027a2f191225f0",
+    measurementId: "G-HBYTYZBV6B"
+  };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+export { app, auth};
+
 // SIDEBAR RESPONSIVE
 const sidebar = document.getElementById('sidebar')
 
@@ -7,6 +24,7 @@ function toggleSidebar() {
     sidebar.classList.toggle('show')
 }
 
+// ===========================================================================================================
 // SIDEBAR SELECTOR FOR HOVER
 const sidebarLinks = document.querySelectorAll('aside ul li a');
 
