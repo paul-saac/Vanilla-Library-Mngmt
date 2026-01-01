165ee8d493382ff4bcb8c8fa2b7a10f368d41856
paul-saac <johnpaul.saac.legaspi@gmail.com>
2026-01-01 23:42:46 +0800

    Fetching 1st try

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import { auth } from "./shared/scripts/firebaseConfig.js";
+import { db, auth } from "./shared/scripts/firebaseConfig.js";
 import { signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
 import { collection, addDoc, onSnapshot, deleteDoc, doc, updateDoc, serverTimestamp, query, orderBy, setDoc } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
--- pages/books/books.js
+++ pages/books/books.js
@@ -1,4 +1,25 @@
 import { db } from "./shared/scripts/firebaseConfig.js";
+import { collection, addDoc, onSnapshot, deleteDoc, doc, updateDoc, serverTimestamp, query, orderBy, setDoc } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
+const tableBody = document.querySelector("#booksTable tbody");
+async function loadBooks() {
+  const querySnapshot = await getDocs(collection(db, "Books"));
 
+  querySnapshot.forEach((doc) => {
+    const book = doc.data();
 
+    const row = `
+      <tr>
+        <td>${book.Tile}</td>
+        <td>${book.Publisher}</td>
+        <td>${book.Date}</td>
+        <td>${book.Price}</td>
+        <td>${book.Status}</td>
+      </tr>
+    `;
+
+    tableBody.innerHTML += row;
+  });
+}
+
+loadBooks();
