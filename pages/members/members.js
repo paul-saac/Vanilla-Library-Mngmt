000ac88e6b762072030530736426054705758dba
paul-saac <johnpaul.saac.legaspi@gmail.com>
2026-01-05 23:37:46 +0800

    java members 1st

--- pages/members/members.js
+++ pages/members/members.js
@@ -1,1 +1,2 @@
-console.log("e[e[e")
+import { db } from "../../shared/scripts/firebaseConfig.js";
+import { collection, getDocs, addDoc, onSnapshot, deleteDoc, doc, updateDoc, serverTimestamp, query, orderBy, setDoc } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
