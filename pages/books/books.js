765f0cf208676fe55d49a914505a4dda29162f8c
paul-saac <johnpaul.saac.legaspi@gmail.com>
2026-01-06 03:44:25 +0800

    testing

--- pages/books/books.js
+++ pages/books/books.js
@@ -41,5 +41,4 @@ window.addEventListener('hashchange', () => {
     }
 });
 
-
 fetchBooks();
--- pages/students/students.js
+++ pages/students/students.js
@@ -1,5 +1,8 @@
 import { db } from "../../shared/scripts/firebaseConfig.js";
 import { collection, getDocs, addDoc, onSnapshot, deleteDoc, doc, updateDoc, serverTimestamp, query, orderBy, setDoc } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
-
- 
+ function fetchStudents() {
+    getDocs(booksCollection).then(booksSnapshot => {
+        // code here runs after Promise resolves
+    });
+}
