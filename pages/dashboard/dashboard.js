7714ec5a520479088e52800a28fa4b836950e10d
paul-saac <johnpaul.saac.legaspi@gmail.com>
2026-01-23 16:18:45 +0800

    fixing dasshboard elements

--- pages/dashboard/dashboard.js
+++ pages/dashboard/dashboard.js
@@ -1,1 +1,13 @@
-console.log("Dashboard")
+import { db } from "../../shared/scripts/firebaseConfig.js";
+import { collection, getDoc, getDocs, addDoc, onSnapshot, deleteDoc, doc, updateDoc, serverTimestamp, query, orderBy, setDoc } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+
+
+
+
+
+
+
+
+
+
